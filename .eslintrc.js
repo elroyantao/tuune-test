3d332c023a4c824478e4ddb1ee071cb501b52eba
Elroy Antao <elroyantao@gmail.com>
2021-03-28 16:59:06 +0100

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  },
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  plugins: [],
+  rules: {
+    'react/prop-types': 1
+  }
+}
